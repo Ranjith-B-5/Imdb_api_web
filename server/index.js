7294f68d44673907475bfd733438a5e990410e18
Ranjith-B <am.ranju007@gmail.com>
2022-12-18 22:12:55 +0530

    hide api

--- server/index.js
+++ server/index.js
@@ -1,38 +1,41 @@
 const path = require('path');
 const express = require("express");
 const fetch = require('node-fetch');
+require('dotenv').config();
+
 
 const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-// const url = 'https://imdb-top-100-movies.p.rapidapi.com/premiummovies';
+const url = 'https://imdb-top-100-movies.p.rapidapi.com/premiummovies';
 
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Key': '0441ed7b34mshb1e1bf46c8883d3p18ffd2jsnae9ded16ec3b',
-//     'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
-//   }
-// };
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': process.env.API_KEY,
+    'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+  }
+};
 
-// async function getdetails()
-// {
-// const res = await fetch(url, options)
-// 	const json_data = await res.json();
-// 	console.log(json_data);
-//   return json_data;
-// }
+async function getdetails()
+{
+const res = await fetch(url, options)
+	const json_data = await res.json();
+	console.log(json_data);
+  return json_data;
+}
 
-// const movie_details = getdetails();
+const movie_details = getdetails();
   
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 
-// app.get('/', (req, res) => {
-//     console.log(movie_details);
-//     res.send(movie_details);
-// });
+app.get('/', (req, res) => {
+    console.log(movie_details);
+    res.send(movie_details);
+});
 
 
 
